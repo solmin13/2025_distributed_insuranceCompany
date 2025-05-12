b3e47fd6e87edd8193374e0a1a0919cfa29dcf49
jangsoyun <soyoon5328@gmail.com>
2025-05-13 00:38:23 +0900

    Refact: 변수 범위 변경

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,25 +5,28 @@ import java.util.*;
 import main.Employee.EmployeeType;
 
 public class Main {
+
+	private static String loginedEmployeeID;
+	private static Menu menu;
+	private static LoadData loadData;
 	private static CustomerListImpl customerList;
 	private static EmployeeListImpl employeeList;
-	private static String loginedEmployeeID;
 
 	public static void main(String[] args) {
 		customerList = new CustomerListImpl();
 		employeeList = new EmployeeListImpl();
-		LoadData loadData = new LoadData(customerList, employeeList);
+		loadData = new LoadData(customerList, employeeList);
 
 		loadData.loadCustomerData();
-
 		loadData.loadEmployeeData();
 
-		loginedEmployeeID = "1";
-		Menu menu = new Menu(customerList, employeeList,loginedEmployeeID);
+		loginedEmployeeID = "1"; // TODO: implement login
+		
+		Menu menu = new Menu(customerList, employeeList, loginedEmployeeID);
 		while (true) {
 			menu.printMainMenu();
 			int selectedMenu = menu.getUserSelectInt();
-			menu.excuteMenu(selectedMenu);
+			menu.excuteSelectedMenu(selectedMenu);
 		}
 
 	}
