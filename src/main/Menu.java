dc0afe5956004946389b59628d6038efacaead44
CREE <leejongmin774@gmail.com>
2025-05-14 16:36:02 +0900

    payCompensation() 설명 주석

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -435,11 +435,17 @@ public class Menu {
 
 	//----------------LossAdjuster--------------------------------------
 
+	/**
+	 * search관련 메소드 분리되지 않음
+	 */
 	private void payCompensation(){
-		LossAdjuster lossAdjuster = (LossAdjuster) loginedEmployee;
-		EventList eventList = lossAdjuster.getEventList();
+		LossAdjuster lossAdjuster = (LossAdjuster) loginedEmployee; //관리자 로딩
+
+		EventList eventList = lossAdjuster.getEventList(); //컴포지션... 관리자가 리스트를 들고 있음, 가져와야함
+
+		//보상 지급 대기중인 보상 조회 로직, 라인넘버 통해서 선택함,
 		System.out.println("===CompensationList===");
-		ArrayList<Compensation> compensations = eventList.searchCompensation("state","Awaiting");
+		ArrayList<Compensation> compensations = eventList.searchCompensation("state","Awaiting"); // 일반 보상 지급이 아직 되지 않은 경우만 골라오긴 하는데, 보상 지급 결정이 내려졌는지가 반영이 되야할것같음.. DB 마렵네
 		for(int i = 0; i<compensations.size(); i++){
 			Compensation targetCompensation = compensations.get(i);
 			System.out.println((i+1)+": Customer:"+targetCompensation.getCustomerID()+", Amount charged: "+targetCompensation.getAmountOfPaid());
@@ -448,6 +454,8 @@ public class Menu {
 		Compensation selectedCompensation = compensations.get(getUserSelectInt());
 		Evaluation selectedEvaluation = eventList.searchEvaluation("id",selectedCompensation.getEvaluationID()).getFirst();
 		Event selectedEvent = eventList.searchEvent("id",selectedEvaluation.getEventID()).getFirst();
+
+		//상세정보 표시 및 보상 지급 선택
 		System.out.println("==상세정보==\n"+selectedEvent+", Amount charged: "+selectedEvaluation.getCompensation().getAmountOfPaid());
 		System.out.println("보상을 지급하시겠습니까?");
 		switch (getUserSelectYorN()){
@@ -462,7 +470,6 @@ public class Menu {
 				break;
 		}
 	}
-
 	private void evaluateCompensation(){
 
 	}
